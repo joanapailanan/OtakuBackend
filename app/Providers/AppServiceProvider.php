32fe4720d6125c993ffbb8c5ac09816d587958db
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-05-24 17:34:55 +0800

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Services\AnilistService;
 use App\Services\JikanApiService;
 
 class AppServiceProvider extends ServiceProvider
 {
-
     public function register()
     {
         $this->app->singleton(JikanApiService::class, function ($app) {
             return new JikanApiService();
         });
+    }
+
+    public function boot()
+    {
+        // Force HTTPS in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
